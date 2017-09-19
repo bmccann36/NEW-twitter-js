0871a4001e655193934d7b97df62ab7b09a77749
Brian McCann <bmccann36@gmail.com>
2017-09-19 13:06:21 -0400

    before lunch

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express(); // creates an instance of an express application
-
+const nunjucks = require('nunjucks')
+const modules = require('./routes')
 // let news = ''
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     // console.log(output);
+// });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 var myLogger = function(req, res, next) {
     console.log('LOGGED')
@@ -10,8 +27,13 @@ var myLogger = function(req, res, next) {
 
 app.use(myLogger)
 
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
 app.get('/', function(req, res) {
-    res.send('hello world')
+    // res.send('hello world')
+
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 app.get('/news', function(req, res) {
